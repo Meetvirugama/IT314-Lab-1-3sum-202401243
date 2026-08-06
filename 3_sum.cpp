dab6b4aec59a80f755e7237f16ca3bdbe4e5bf0e
Meet Virugama <meetvirugama@Meets-MacBook-Air.local>
2026-08-06 18:09:03 +0530

    Update 3_sum.cpp

--- 3_sum.cpp
+++ 3_sum.cpp
@@ -1,10 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
+
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size(); i++) {
+
 
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
@@ -18,6 +26,7 @@ public:
 
                 if (sum == 0) {
                     ans.push_back({nums[i], nums[left], nums[right]});
+
                     left++;
                     right--;
 
@@ -39,3 +48,51 @@ public:
         return ans;
     }
 };
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> testCases = {
+        {-1, 0, 1, 2, -1, -4},
+        {0, 0, 0, 0},
+        {1, 2, -2, -1},
+        {-2, 0, 1, 1, 2},
+        {-4, -1, -1, 0, 1, 2, 2},
+        {3, -2, -1, 0, 1, 2, -3},
+        {5, -5, 0},
+        {1, 1, -2},
+        {0, 1, 1},
+        {}
+    };
+
+    for (int t = 0; t < testCases.size(); t++) {
+
+        cout << "Test Case " << t + 1 << ":\n";
+
+        vector<int> nums = testCases[t];
+
+        vector<vector<int>> ans = sol.threeSum(nums);
+
+        if (ans.empty()) {
+            cout << "No triplets found";
+        }
+        else {
+            for (auto &triplet : ans) {
+                cout << "[";
+
+                for (int i = 0; i < triplet.size(); i++) {
+                    cout << triplet[i];
+
+                    if (i != triplet.size() - 1)
+                        cout << ", ";
+                }
+
+                cout << "] ";
+            }
+        }
+
+        cout << "\n\n";
+    }
+
+    return 0;
+}
