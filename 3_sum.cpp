77d7eb28155c0b19b147baa549befa39c53cb14d
Meet Virugama <meetvirugama@Meets-MacBook-Air.local>
2026-08-06 17:57:16 +0530

    Fix duplicate handling for right pointer (Fixes #1)

--- 3_sum.cpp
+++ 3_sum.cpp
@@ -24,7 +24,7 @@ public:
                     while (left < right && nums[left] == nums[left - 1])
                         left++;
 
-                    while (left < right && nums[right] == nums[right - 1])
+                    while (left < right && nums[right] == nums[right + 1])
                         right--;
                 }
                 else if (sum < 0) {
